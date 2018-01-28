94922f138c07afbfbe0ab734090e49f57f689acc
Gil Sousa <gsousa@ventureoak.com>
2018-01-28 12:39:49 +0000

    Fix PEP8

--- locators/LoginPageLocators.py
+++ locators/LoginPageLocators.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 
+
 class LoginPageLocators(object):
     USR_ELEM = (By.CSS_SELECTOR, '#m_login_email')
     PWD_ELEM = (By.CSS_SELECTOR, '#login_form > ul > li:nth-child(2) > div > input')
